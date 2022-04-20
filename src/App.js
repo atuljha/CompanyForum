461f92d1c60ec286b548d158859d768b9163aa69
charitreddy <charitreddy@gmail.com>
2022-04-20 15:50:10 -0400

    UI Changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from "react"
-import Home from "./components/Home"
-import "./App.css"
+import React from "react";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import "./App.css";
 
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Login from "./components/Login"
-import Signup from "./components/Signup"
-export default function App(){
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+export default function App() {
   return (
     <Router>
-       <Routes>
-         <Route path ="/" element={<Login />}/>
-         <Route path ="/signup" element={<Signup />}/>
-         <Route path ="/home" element={<Home />}/>
-       </Routes>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
     </Router>
-  )
+  );
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,74 +1,91 @@
-import { Button } from "@material-ui/core"
-import React from "react"
-import { useNavigate } from "react-router-dom"
-import "./Login.css"
-import UserService from "../services/UserService"
+import { Button } from "@material-ui/core";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
+import UserService from "../services/UserService";
 
+export default function Signup() {
+  const [data, setData] = React.useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-export default function Signup(){
+  const navigate = useNavigate();
 
-    const[data, setData] = React.useState({
-        firstName:"",
-        lastName:"",
-        email:"",
-        password:"",
-        confirmPassword:""
-    })
+  function handleChange(event) {
+    const { name, value } = event.target;
 
-    const navigate = useNavigate();
+    setData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
 
-    function handleChange(event){
+  function handleClick() {
+    UserService.create(data).then((res) => {
+      console.log(res.data);
+      localStorage.setItem("id", res.data._id);
+      localStorage.setItem(
+        "name",
+        `${res.data.firstName} ${res.data.lastName}`
+      );
 
-        const {name, value} = event.target
+      navigate("/home");
+    });
+  }
 
-        setData(prev=> ({
-             
-                ...prev,
-                [name]:value
-            }))
-    }
+  return (
+    <div className='login signup'>
+      <div className='signup_form'>
+        <div className='signup_final'>
+          <h2>SIGN UP</h2>
 
-    function handleClick(){
-        UserService.create(data)
-        .then(res=>{
-            console.log(res.data)
-            localStorage.setItem("id", res.data._id)
-            localStorage.setItem("name", `${res.data.firstName} ${res.data.lastName}` )
-
-            navigate("/home")
-        })
-    }
-
-    return(
-        <div className="login">
-        <input 
-         type="text"
-         placeholder="First Name"
-         name="firstName"
-         value={data.firstName}
-         onChange={handleChange}
-         />
-        <input 
-        type="text"
-         placeholder="Last Name"
-         name="lastName"
-         value={data.lastName}
-         onChange={handleChange}
-         />
-        <input 
-        type="text"
-         placeholder="Email"
-         name="email"
-         value={data.email}
-         onChange={handleChange}
-         />
-        <input 
-        type="password"
-         placeholder="Password"
-         name="password"
-         value={data.password}
-         onChange={handleChange}/> 
-        <Button onClick={handleClick}> Signup</Button>
+          <input
+            type='text'
+            placeholder='First Name'
+            name='firstName'
+            value={data.firstName}
+            onChange={handleChange}
+          />
+          <input
+            type='text'
+            placeholder='Last Name'
+            name='lastName'
+            value={data.lastName}
+            onChange={handleChange}
+          />
+          <input
+            type='text'
+            placeholder='Email'
+            name='email'
+            value={data.email}
+            onChange={handleChange}
+          />
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={data.password}
+            onChange={handleChange}
+          />
+          <Button
+            variant='outlined'
+            onClick={handleClick}
+            style={{
+              color: "white",
+              border: "2px solid white",
+              marginTop: "40px",
+              marginBottom: "20px",
+              backgroundColor: "green",
+            }}>
+            {" "}
+            Signup
+          </Button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
